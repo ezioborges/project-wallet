ec971885f305a7b1a79da1c4dbd72efc2ce76096
Ezio Borges <eziopborges@gmail.com>
2023-10-28 16:57:13 -0300

    ajustes na tela de login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -59,55 +59,59 @@ function Login() {
       className="d-flex justify-content-center align-items-center background-content"
       style={{ minHeight: "100vh" }}
     >
-      <form className="w-50 p-5 background-form-ligin">
-        <div className="form-group mb-4">
-          <label htmlFor="username">Nome</label>
-          <input
-            type="text"
-            name="username"
-            value={userData.username}
-            className="form-control mt-2"
-            aria-describedby="emailHelp"
-            placeholder="Digite seu nome..."
-            onChange={handleChange}
-          />
+      <div className="row w-75">
+        <div className="col-12">
+          <form className="p-5 background-form-ligin">
+            <div className="form-group mb-4">
+              <label htmlFor="username">Nome</label>
+              <input
+                type="text"
+                name="username"
+                value={userData.username}
+                className="form-control mt-2"
+                aria-describedby="emailHelp"
+                placeholder="Digite seu nome..."
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-group mb-4">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                value={userData.email}
+                className="form-control mt-2"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Digite seu email..."
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-group mb-4">
+              <label htmlFor="password">Senha</label>
+              <input
+                type="password"
+                name="password"
+                value={userData.password}
+                className="form-control mt-2"
+                id="exampleInputPassword1"
+                placeholder="Digite sua senha..."
+                onChange={handleChange}
+              />
+            </div>
+            <div className="d-flex justify-content-center">
+              <button
+                type="submit"
+                className="btn btn-primary"
+                disabled={userData.disabled}
+                onClick={handleClick}
+              >
+                Entrar
+              </button>
+            </div>
+          </form>
         </div>
-        <div className="form-group mb-4">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            name="email"
-            value={userData.email}
-            className="form-control mt-2"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-            placeholder="Digite seu email..."
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-group mb-4">
-          <label htmlFor="password">Senha</label>
-          <input
-            type="password"
-            name="password"
-            value={userData.password}
-            className="form-control mt-2"
-            id="exampleInputPassword1"
-            placeholder="Digite sua senha..."
-            onChange={handleChange}
-          />
-        </div>
-        <div className="d-flex justify-content-center">
-          <button
-            type="submit"
-            className="btn btn-primary"
-            disabled={userData.disabled}
-            onClick={handleClick}
-          >
-            Entrar
-          </button>
-        </div>
-      </form>
+      </div>
     </div>
   );
 }
